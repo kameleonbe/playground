15ad604b296a6c4dd05cdfc54ffb2c185e91b28f
kameleonbe <info@kameleon-media.be>
2015-03-27 18:04:10 +0100

    Changed index.php

--- index.php
+++ index.php
@@ -3,12 +3,10 @@
 
 <div class='container'>
     <div class='row'>
-<form action="php/process.php" method="POST">
-
-	<input type='submit' value='Maak nieuwe user aan'/> 
-</form>
-</div>
+        <form action="php/process.php" method="POST">
+               <input type='submit' value='Maak nieuwe user aan'/> 
+        </form>
     </div>
-
+</div>
 
 <?php include ('include/footer.php'); ?>
