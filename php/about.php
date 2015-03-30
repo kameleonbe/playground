39a7d92cc87d87bf063cebaad0e95fb4d9738b12
kameleonbe <info@kameleon-media.be>
2015-03-30 09:15:35 +0200

    Updated READ.md

--- php/about.php
+++ php/about.php
@@ -4,7 +4,7 @@
 		<div class="row">
                     <div class="col-md-6">
                         <div class="row">
-                        <p>I finally decided to teach myself PHP better. Should have done this 15 years ago (better late then never). Yes I am a front-end developer and Angular.js and Node.js are more then buzz words, but I love Drupal. And Drupal.. . well it's gonna be PHP all over the place for at least the near future. Drupal 8 coming and it not written in node.js or .NET. So.</p>
+                        <p>I finally decided to teach myself PHP better. Should have done this 15 years ago (better late then never). Yes I am a front-end developer and Angular.js and Node.js are more then buzz words, but I love Drupal. And Drupal.. . well it's gonna be PHP all over the place for at least the near future. Drupal 8 coming and it's not written in node.js or .NET. So.</p>
                         <p>Romain - march 2015</p>
                         </div>
                     </div>
