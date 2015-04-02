9ae92275c4591d6300eb61a4e1ec570842168344
kameleonbe <info@kameleon-media.be>
2015-04-02 10:37:00 +0200

    Recovered lost process.php

--- php/process.php
+++ php/process.php
@@ -4,23 +4,20 @@ $ctn = 1;
 $table = $_REQUEST ['table'];
 ?>
 <div id="main">
-	<div class="container">
-		<div class="row">
-                    <div class="col-md-12">
-			<div class="jumbotron">
-                            <h2><?php echo ('Tafel van ' . $table ) ?></h2>
-                            <?php
-                                    while ($ctn < 11) {
-                                            echo ($table . " x " . $ctn .  " = " . $ctn * $table . '<br />');
-                                            $ctn++;
-                                    }
-                            ?>
-			</div>
-                    </div>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="jumbotron">
+                    <h2><?php echo ('Tafel van ' . $table ) ?></h2>
+                    <?php
+                    while ($ctn < 11) {
+                        echo ($table . " x " . $ctn . " = " . $ctn * $table . '<br />');
+                        $ctn++;
+                    }
+                    ?>
+                </div>
             </div>
-	</div>
+        </div>
+    </div>
 </div>
-
 <?php include ('../include/footer.php'); ?>
-
-
