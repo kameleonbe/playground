59161f587b97e722835f9b84382bdde90a0fdc44
kameleonbe <info@kameleon-media.be>
2015-04-02 10:35:39 +0200

    Recovered lost process.php

---
+++ php/process.php
@@ -0,0 +1,26 @@
+<?php include ('../include/header.php'); ?>
+<?php	
+$ctn = 1;
+$table = $_REQUEST ['table'];
+?>
+<div id="main">
+	<div class="container">
+		<div class="row">
+                    <div class="col-md-12">
+			<div class="jumbotron">
+                            <h2><?php echo ('Tafel van ' . $table ) ?></h2>
+                            <?php
+                                    while ($ctn < 11) {
+                                            echo ($table . " x " . $ctn .  " = " . $ctn * $table . '<br />');
+                                            $ctn++;
+                                    }
+                            ?>
+			</div>
+                    </div>
+            </div>
+	</div>
+</div>
+
+<?php include ('../include/footer.php'); ?>
+
+
