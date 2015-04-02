5a6b8f7a64e8037410e7f9c18bb2b841f306dcf2
kameleonbe <info@kameleon-media.be>
2015-04-02 14:05:57 +0200

    Added bootstrap menu Added bootstrap.min.js Added jquery Minor formatting

--- include/header.php
+++ include/header.php
@@ -5,32 +5,35 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link href="../assets/css/bootstrap.css" media="screen" rel="stylesheet" type="text/css" />
         <link href="../assets/css/style.css" media="screen" rel="stylesheet" type="text/css" />
+        <script src="../assets/js/jquery-1.11.2.min.js"></script>
+        <script src="../assets/js/bootstrap.min.js"></script>
     </head>
 	<body>
 	<header role="banner" id="top" class="navbar navbar-static-top bs-docs-nav">
-	  <div class="container">
-	    <div class='row'>
-		<div class="navbar-header">
-	      <button data-target=".bs-navbar-collapse" data-toggle="collapse" type="button" class="navbar-toggle collapsed">
-	        <span class="sr-only">Toggle navigation</span>
-	        <span class="icon-bar"></span>
-	        <span class="icon-bar"></span>
-	        <span class="icon-bar"></span>
-	      </button>
-	      <a class="navbar-brand" href="../">PHP Playground</a>
-	    </div>
-	    <nav class="collapse navbar-collapse bs-navbar-collapse">
-	      <ul class="nav navbar-nav">
-	        <li>
-	          <a href="/">Index</a>
-	        </li>
-	      </ul>
-	      <ul class="nav navbar-nav navbar-right">
-	        <li><a href="../php/phpinfo.php">phpinfo()</a></li>
-	        <li><a href="../php/about.php">About</a></li>
-                <li><a href="https://github.com/kameleonbe/playground">GitHub</a></li>
-	      </ul>
-	    </nav>
-		</div>
-	  </div>
-	</header>
+    <nav class="navbar navbar-default">
+      <div class="container-fluid">
+        <!-- Brand and toggle get grouped for better mobile display -->
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="/">PHP Playground</a>
+        </div>
+
+        <!-- Collect the nav links, forms, and other content for toggling -->
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+          <ul class="nav navbar-nav">
+            <li><a href="/">Index</a></li>
+          </ul>
+          <ul class="nav navbar-nav navbar-right">
+             <li><a href="../php/phpinfo.php">phpinfo()</a></li>
+             <li><a href="../php/about.php">About</a></li>
+             <li><a href="https://github.com/kameleonbe/playground">GitHub</a></li>
+          </ul>
+        </div><!-- /.navbar-collapse -->
+      </div><!-- /.container-fluid -->
+    </nav>
+</header>
--- php/about.php
+++ php/about.php
@@ -1,28 +1,23 @@
 <?php include ('../include/header.php'); ?>
 <div id="main">
-	<div class="container">
-		<div class="row">
-                    <div class="col-md-6">
-                        <div class="row">
-                        <p>I finally decided to teach myself PHP better. Should have done this 15 years ago (better late then never). Yes I am a front-end developer and Angular.js and Node.js are more then buzz words, but I love Drupal. And Drupal.. . well it's gonna be PHP all over the place for at least the near future. Drupal 8 coming and it's not written in node.js or .NET. So.</p>
-                        <p>Romain - march 2015</p>
-                        </div>
-                    </div>
-                    
-                    <div class="col-md-6">
-                        <div class="row">
-                            <p>Contact me: <a href='mailto:info@kameleon-media.be'>info@kameleon-media.be</a></p>
-                        <ul>
-                            <li>Drupal</li>
-                            <li>CSS/Sass</li>
-                            <li>HTML</li>
-                            <li>Git</li>
-                            <li>Javascript</li>
-                            <li>PHP</li> 
-                        </ul>
-                        </div>
-                    </div>
-                 </div>
-	</div>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6">
+                <p>I finally decided to teach myself PHP better. Should have done this 15 years ago (better late then never). Yes I am a front-end developer and Angular.js and Node.js are more then buzz words, but I love Drupal. And Drupal.. . well it's gonna be PHP all over the place for at least the near future. Drupal 8 coming and it's not written in node.js or .NET. So.</p>
+                <p>Romain - march 2015</p>
+            </div>
+
+            <div class="col-md-6">
+                <p>Contact me: <a href='mailto:info@kameleon-media.be'>info@kameleon-media.be</a></p>
+                <ul>
+                    <li>Drupal</li>
+                    <li>CSS/Sass</li>
+                    <li>HTML</li>h
+                    <li>Git</li>
+                    <li>Javascript</li>
+                    <li>PHP</li> 
+            </div>
+        </div>
+    </div>
 </div>
 <?php include ('../include/footer.php'); ?>
