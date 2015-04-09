9ea897b0e572f09bce044339a3728a3204bb207d
kameleonbe <info@kameleon-media.be>
2015-04-09 12:46:45 +0200

    Added .htaccess with example of including header and footer in .htaccess Added angular.js CDN

--- include/header.php
+++ include/header.php
@@ -7,6 +7,7 @@
         <link href="../assets/css/style.css" media="screen" rel="stylesheet" type="text/css" />
         <script src="../assets/js/jquery-1.11.2.min.js"></script>
         <script src="../assets/js/bootstrap.min.js"></script>
+        <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.3.15/angular.min.js"></script>
     </head>
 	<body>
 	<header role="banner" id="top" class="navbar navbar-static-top bs-docs-nav">
