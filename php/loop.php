41fca4c1dccc79a80ba9b80b2a0d53a3c94952c6
kameleonbe <info@kameleon-media.be>
2015-04-09 12:40:14 +0200

    Added foreach second example with assoc array in loop.php Minor css changes

--- php/loop.php
+++ php/loop.php
@@ -69,7 +69,44 @@ foreach ($for_each_array as $item) {
                     echo $item . '<br />';
                 }
             ?>
-            
+            <hr>
+            <h4>foreach 2</h4>
+            <code>
+<pre>
+$icons = array(
+'thumbs-up' => array ('thumbs-up','http://www.kameleon-media.be','go to kameleon.media'),
+'thumbs-down' => array ('thumbs-down','http://www.kameleon-media.eu','go to kameleon.media'),
+'hand-right' => array ('hand-right','http://kameleon.media','go to kameleon.media'),
+'hand-left' => array ('hand-left','http://www.kameleon-media.be','go to kameleon.media'),
+'hand-up' => array ('hand-up','http://www.kameleon-media.eu','go to kameleon.media'),
+'hand-down' => array ('hand-down','http://kameleon.media','go to kameleon.media')
+);
+
+$lengte = count($icons);
+foreach ($icons as $icon) :
+<a href="echo $icon[1];" title="echo $icon[2];"><span class="glyphicon glyphicon- echo $icon[0];"></span></a>
+endforeach;
+</pre>
+            </code>
+
+            <?php
+                $icons = array(
+                    'thumbs-up' => array ('thumbs-up','http://www.kameleon-media.be','go to kameleon.media'),
+                    'thumbs-down' => array ('thumbs-down','http://www.kameleon-media.eu','go to kameleon.media'),
+                    'hand-right' => array ('hand-right','http://kameleon.media','go to kameleon.media'),
+                    'hand-left' => array ('hand-left','http://www.kameleon-media.be','go to kameleon.media'),
+                    'hand-up' => array ('hand-up','http://www.kameleon-media.eu','go to kameleon.media'),
+                    'hand-down' => array ('hand-down','http://kameleon.media','go to kameleon.media')
+                 );
+                
+                $lengte = count($icons);
+                
+                foreach ($icons as $icon) :
+            ?>
+            <a href="<?php echo $icon[1]; ?>" title="<?php echo $icon[2]; ?>"><span class="glyphicon glyphicon-<?php echo $icon[0]; ?>"></span></a>
+                
+            <?php endforeach;?>
+     
             </div>
         </div>
     </div>
