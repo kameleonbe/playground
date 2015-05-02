d36fc12b4797efce2f2c409d5c9e15898d24fdfb
kameleonbe <info@kameleon-media.be>
2015-05-02 12:24:05 +0200

    created  builtinfunctions.php added link to footer.php

---
+++ php/builtinfunctions.php
@@ -0,0 +1,25 @@
+<?php include ('../include/header.php'); ?>
+<div id="main">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <a href="http://php.net/manual/en/functions.internal.php">http://php.net/manual/en/functions.internal.php</a>
+                <h4>strlen()</h4>
+                <code>
+<pre>
+echo strlen('Dara Factor Two');
+</pre>
+                </code>
+
+                <section>
+                        <?php
+                            echo 'String length of Dara Factor Two = ' . strlen('Dara Factor Two') . '.';
+                        ?>
+                </section>
+                <hr>
+                
+            </div>
+        </div>
+    </div>
+</div>
+<?php include ('../include/footer.php'); ?>
