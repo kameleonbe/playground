24de3716ba7ebc4edea62e21c1a1c8a6e20ec450
kameleonbe <info@kameleon-media.be>
2015-05-02 18:10:26 +0200

    added substr() and strpos()

--- php/builtinfunctions.php
+++ php/builtinfunctions.php
@@ -4,16 +4,32 @@
         <div class="row">
             <div class="col-md-12">
                 <a href="http://php.net/manual/en/functions.internal.php">http://php.net/manual/en/functions.internal.php</a>
-                <h4>strlen()</h4>
+                <h4>strlen(), substr(), strpos()</h4>
                 <code>
 <pre>
-echo strlen('Dara Factor Two');
+$phrase = 'Dara Factor Two';
+// strlen
+echo 'String length of Dara Factor Two = ' . strlen($phrase);
+// substr
+echo 'Substring (0,8) = ' . substr($phrase,0,8);
+// strpos
+$start = strpos($phrase,'Factor');
+echo 'Strpos of Factor = ' . $start;
+echo 'Find sentence starting from Factor = ' . substr($phrase,$start);
 </pre>
                 </code>
 
                 <section>
                         <?php
-                            echo 'String length of Dara Factor Two = ' . strlen('Dara Factor Two') . '.';
+                            $phrase = 'Dara Factor Two';
+                            // strlen
+                            echo 'String length of Dara Factor Two = ' . strlen($phrase) . '</br>';
+                            // substr
+                            echo 'Substring (0,8) = ' . substr($phrase,0,8) . '</br>';
+                            // strpos
+                            $start = strpos($phrase,'Factor');
+                            echo 'Strpos of Factor = ' . $start . '</br>';
+                            echo 'Find sentence starting from Factor = ' . substr($phrase,$start);
                         ?>
                 </section>
                 <hr>
